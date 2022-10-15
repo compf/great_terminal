0999c01c393b4126070a8274141ecb3c5253bf34
timo.schoemaker <timo.schoemaker@hotmail.de>
2022-10-15 19:54:52 +0200

    c

--- gui/table_view_creator.py
+++ gui/table_view_creator.py
@@ -1,20 +1,23 @@
-from PyQt5.QtWidgets import QTableView
+from PyQt5.QtWidgets import QTableView,QGridLayout,QLabel,QLayout
 from PyQt5.QtGui import QStandardItemModel,QStandardItem
 from typing import List
 from output_parsing.terminal_output_builder import TerminalOutputBuilder
-def create_table_view(table_parser:List[List[str]]) ->QTableView:
-    table_view=QTableView()
+def create_table_view(table_parser:List[List[str]]) ->QGridLayout:
+    grid_view=QGridLayout()
+    grid_view.setSizeConstraint(QLayout.SizeConstraint.SetMaximumSize);
+    #grid_view.setContentsMargins(10,10,10,10)
     item_model=QStandardItemModel()
     y=0
     x=0
     print("windows",table_parser)
     for r in table_parser:
         for c in r:
-            print("qd",c)
-            item=QStandardItem(c)
-            x+=1
-            item_model.setItem(y,x,item)
+            lbl=QLabel()
+            lbl.setText(c)
+            lbl.setFixedHeight(20)
+      
+            grid_view.addWidget(lbl,y,x)
+            grid_view.setRowMinimumHeight(y,30)
         x=0
         y+=1
-    table_view.setModel(item_model)
-    return table_view
+    return grid_view
